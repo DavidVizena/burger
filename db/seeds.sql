fbc428a3742deaf84368c5dd3f6849884328c5ca
Vizena2005 <vizena2005@gmail.com>
2018-04-30 10:19:53 -0500

    initializing

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Blackened Chicken');
+INSERT INTO burgers (burger_name) VALUES ('Patty Melt');
