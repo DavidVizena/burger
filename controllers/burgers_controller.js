488ba54b172fb3a37c6e315d26d4861ac7d4e864
Vizena2005 <vizena2005@gmail.com>
2018-05-02 09:45:46 -0500

    i think i have everything linked correctly now, it all seems to be able to log correctly

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,29 @@
 var express = require('express'),
-    burger = require('burger.js');
+    burger = require('../models/burger'),
+    router = express.Router();
 
-    // SET UP FOR ROUTER
+router.get('/', (req, res) => {
+    burger.selectALL(function (data) {
+        var dataObject = {
+            burgers: data
+        };
+
+        for (var i = 0; i < dataObject.burgers.length; i++) {
+            dataObject.burgers[i].devoured = parseInt
+            (dataObject.burgers[i].devoured);
+        }
+        res.render('index', dataObject);
+    });
+});
+
+router.post('/', (req, res) => {
+    burger.insertOne(req.body.burgerInput);
+    res.redirect('/');
+});
+
+router.put('/:id', (req, res) => {
+    burger.updateOne(req.params.id);
+    res.redirect('/');
+});
+
+module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,17 @@
-var orm = require('"./config/orm.js"');
+var orm = require('./../config/orm');
 
-// NEED TO SET UP ORM CALLS INSTRUCTIONS: MODEL SETUP
+var burger = {
+    selectALL: function (cbk) {
+        orm.selectAll('burgers', function (data) {
+            cbk(data);
+        });
+    },
+    insertOne: function (burgerName) {
+        orm.insertOne('burgers', 'burger_name', 'devoured', burgerName, '0');
+    },
+    updateOne: function (id) {
+        orm.updateOne('burgers', 'devoured', '1', 'id', id);
+    }
+};
+
+module.exports = burger;
