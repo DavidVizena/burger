2a88a4eaadc5d195d340dd3614ed7d511e0f1468
Vizena2005 <vizena2005@gmail.com>
2018-05-02 09:11:20 -0500

    config done

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,17 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+var mysql = require('mysql'),
+    connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_HOST
+    });
 
-var app = express();
-
-var PORT = process.env.PORT || 8080;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
+connection.connect(function(err){
+  if(err){
+    console.error('Error connecting: ' + err.stack);
     return;
   }
+  console.log('Conected as ID ' + connection.threadId);
+})
 
-  console.log("connected as id " + connection.threadId);
-});
-
-module.exports = connection;
+module.exports =  connection;
--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,25 @@
-var connection = require('connection.js'),
-    orm = selectAll();
-    insertOne();
-    updateOne();
+var connection = require('./connection'),
+    orm = {
 
-module.exports =  orm;
+        selectAll: (table, cbk) => {
+            var queryString = 'SELECT * FROM ??';
+            connection.query(queryString, [table], (err, result) => {
+                if (err) throw err;
+                cbk(result);
+            });
+        },
+        insertOne: (table, column1, column2, value1, value2) => {
+            var queryString = 'INSERT INTO ?? (??, ??) VALUES (?, ?)';
+            connection.query(queryString, [table, column1, column2, value1, value2], (err) => {
+                if (err) throw err;
+            });
+        },
+        updateOne: (table, column1, value1, column2, value2) => {
+            var queryString = 'UPDATE ?? SET ?? = ? WHERE ?? = ?';
+            connection.query(queryString, [table, column1, value1, column2, value2], (err) => {
+                if (err) throw err;
+            });
+        }
+    };
+
+module.exports = orm;
