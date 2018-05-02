b2fcce8416c483b81cdb186193ff40677169d1cf
Vizena2005 <vizena2005@gmail.com>
2018-05-02 12:13:13 -0500

    finished, going to get christo show me how to be more secure

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 var mysql = require('mysql'),
-    connection = mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_HOST
-    });
+  // connection = mysql.createConnection({
+  //   host: process.env.DB_HOST,
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASS,
+  //   database: process.env.DB_HOST
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  });
 
-connection.connect(function(err){
-  if(err){
+connection.connect(function (err) {
+  if (err) {
     console.error('Error connecting: ' + err.stack);
     return;
   }
   console.log('Conected as ID ' + connection.threadId);
 })
 
-module.exports =  connection;
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,2 +1,28 @@
 var express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    exphbs = require('express-handlebars'),
+    path = require('path'),
+    methodOverride = require('method-override'),
+    dotenv = require('dotenv').config(),
+
+    app = express(),
+    public = require('path').join(__dirname, '/public');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(public));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+var routes = require('./controllers/burgers_controller');
+
+var PORT = process.env.PORT || 3000;
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
